77ab90b5c17dba62763eb525969c0327da3c55a8
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-11-30 21:52:24 +0300

    Use usecontext

--- github-finder/src/Components/Users/UserResults.jsx
+++ github-finder/src/Components/Users/UserResults.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import Spinner from '../Spinner'
 import UserItem from './UserItem'
+import GithubContext from '../../Context/GithubContext'
 
 function UserResults() {
+    const {users, loading, fetchUsers} = useContext(GithubContext)
+    
     useEffect(() => {
-        fetchUsers()
-        
+        fetchUsers()       
     }, [])
 
     if(!loading) {
