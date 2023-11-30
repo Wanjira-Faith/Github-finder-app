546f6fa88e083605ed5554a1bfc2c9c7fcad8310
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-11-30 20:02:04 +0300

    import spinner

--- github-finder/src/Components/UserResults.jsx
+++ github-finder/src/Components/UserResults.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import Spinner from './Spinner'
 
 function UserResults() {
     const [users, setUsers] = useState([])
@@ -31,7 +32,7 @@ function UserResults() {
             </div>
           )
     } else {
-        return <h3>Loading...</h3>
+        return <Spinner/>
     }
 }
 
