73f270b26a34710677e33227b221b8cdb26616d0
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-11-30 21:45:27 +0300

    set up github context

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -4,23 +4,26 @@ import Footer from './Components/Footer';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import NotFound from './Components/Pages/NotFound';
+import { GithubProvider } from './Context/GithubContext';
 
 function App() {
   return (
-    <Router>
-    <div className="flex flex-col justify-between h-screen">
-      <Navbar/>
-     <main className='container mx-auto px-3 pb-12'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/notfound' element={<NotFound/>} />
-        <Route path='/*' element={<NotFound/>} />
-      </Routes>
-     </main>
-     <Footer/>
-     </div>
-  </Router>
+    <GithubProvider>
+     <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar/>
+         <main className='container mx-auto px-3 pb-12'>
+           <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/notfound' element={<NotFound/>} />
+            <Route path='/*' element={<NotFound/>} />
+         </Routes>
+         </main>
+         <Footer/>
+      </div>
+    </Router>
+  </GithubProvider>
   );
 }
 
