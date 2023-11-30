fb079e326d7c1b857c7045a807920407fbb33eef
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-11-30 15:39:09 +0300

    create pages and add home page and routes in app.js

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,21 @@
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Home from './Components/Pages/Home';
 
 function App() {
   return (
     <Router>
     <div className="flex flex-col justify-between h-screen">
       <Navbar/>
-     <main className='container mx-auto px-3 pb-12'>Content</main>
-     
+     <main className='container mx-auto px-3 pb-12'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/notfound' element={<NotFound/>} />
+        <Route path='/*' element={<NotFound/>} />
+      </Routes>
+     </main>
      <Footer/>
      </div>
   </Router>
