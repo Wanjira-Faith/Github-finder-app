f0e98a1c628e38de54c6a0ca03b108ef73feabea
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-01 12:12:59 +0300

    add alert context and reducer

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -5,25 +5,28 @@ import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import NotFound from './Components/Pages/NotFound';
 import { GithubProvider } from './Context/GithubContext';
+import { AlertProvider } from './Context/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
-     <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar/>
-         <main className='container mx-auto px-3 pb-12'>
-           <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/about' element={<About/>} />
-            <Route path='/notfound' element={<NotFound/>} />
-            <Route path='/*' element={<NotFound/>} />
-           </Routes>
-         </main>
-         <Footer/>
-      </div>
-    </Router>
-  </GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar/>
+              <main className='container mx-auto px-3 pb-12'>
+                 <Routes>
+                   <Route path='/' element={<Home/>} />
+                   <Route path='/about' element={<About/>} />
+                   <Route path='/notfound' element={<NotFound/>} />
+                   <Route path='/*' element={<NotFound/>} />
+                 </Routes>
+              </main>
+            <Footer/>
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
 
