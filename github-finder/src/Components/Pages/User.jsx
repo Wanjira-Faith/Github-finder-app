02f1ba3b4d7de95045a59ed91637f546da75fd3f
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-02 16:04:59 +0300

    remove unused facode

--- github-finder/src/Components/Pages/User.jsx
+++ github-finder/src/Components/Pages/User.jsx
@@ -1,4 +1,4 @@
-import { FaCode, FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
+import { FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import Spinner from '../Spinner'
